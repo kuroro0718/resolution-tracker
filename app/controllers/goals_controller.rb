360c5390e2f07e7df75fcffb59bfdf9c8647f40d
Alex Liang <u9211011@gmail.com>
2016-03-23 15:05:18 +0800

    Add partial form for edit and create goal pages

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,6 @@
 class GoalsController < ApplicationController
+  before_action :find_params, only: [:edit, :show, :update, :destroy]
+
   def index
   end
 
@@ -23,6 +25,11 @@ class GoalsController < ApplicationController
   end
 
   def update
+    if @goal.update(goal_params)
+      redirect_to @goal, notice: "修改完成"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -32,5 +39,9 @@ class GoalsController < ApplicationController
 
   def goal_params
     params.require(:goal).permit(:title, :comment, :is_shareable, :complete_date)
+  end
+
+  def find_params
+    @goal = Goal.find(params[:id])
   end
 end
