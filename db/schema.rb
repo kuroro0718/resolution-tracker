cae3dc29650cf48c7577002bf02eb160ce2ae1c8
Alex Liang <u9211011@gmail.com>
2016-03-25 14:50:28 +0800

    Add name to user model and modify mail content

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324082623) do
+ActiveRecord::Schema.define(version: 20160325061715) do
 
   create_table "goals", force: :cascade do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20160324082623) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
